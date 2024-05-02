24562e2aba83e3171dffa04af85e0c7ad75d74fc
bfbrandhorst <bfbrandhorst@gmail.com>
2024-05-02 11:09:12 -0600

    updated notes.js - delete function added

--- routes/notes.js
+++ routes/notes.js
@@ -1,5 +1,6 @@
 const notesRouter = require('express').Router();
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 notesRouter.get('/notes', (req, res) => {
 
@@ -12,4 +13,24 @@ notesRouter.get('/notes', (req, res) => {
     })
 })
 
+notesRouter.post('/notes', (req, res) => {
+    const notesDB = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+    const newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuidv4()
+    }
+    notesDB.push(newNote);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notesDB))
+    res.json(notesDB);
+})
+
+notesRouter.delete('/notes/:id', (req, res) => {
+    const notesDB = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+    const filteredNotes = notesDB.filter((note) => {
+        return note.id !== req.params.id
+    })
+    fs.writeFileSync('./db/db.json', JSON.stringify(filteredNotes))
+    res.json({ message: 'note deleted' })
+})
 module.exports = notesRouter
